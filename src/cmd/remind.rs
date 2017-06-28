67069fb0b62edc172bc2434028cd9aaf0e8e4e49
HeapUnderflow <HeapUnderfl0w@users.noreply.github.com>
2017-06-28 11:30:18 +0000

    Added duration & unit parsing

--- src/cmd/remind.rs
+++ src/cmd/remind.rs
@@ -2,16 +2,32 @@ use serenity::model::Message;
 use serenity::client::Context;
 use chrono::Duration;
 use std::vec::Vec;
-
+#[cfg_attr(freature = "clippy", allow(needless_pass_by_value))]
 pub fn add_reminder(context: &mut Context, message: &Message, args: Vec<String>) -> Result<(), String> {
     if args.len() < 2 {
         return Err("To few arguments".to_owned());
     }
-    let _ = message.reply(&format!("{:?}", args));
+    let ptime = match parse_duration(&args[0]) {
+        Ok(d) => d,
+        Err(e) => return Err(e)
+    };
+    let tmp_message = args[1..].join(" ");
+    let _ = message.reply(&format!("Duration:\n{:#?}\n\nMessage:\n{}", ptime, tmp_message));
     Ok(())
 }
 
-fn parse_duration(string: String) -> Duration {
-    let (duration, unit) = string.split_at(string.len() - 2);
-    Duration::days(0)
+fn parse_duration(string: &str) -> Result<Duration, String> {
+    let (duration, unit) = string.split_at(string.len() - 1);
+    let time = match duration.parse::<u64>() {
+        Ok(v) => v,
+        Err(e) => return Err(format!("{:?}", e))
+    } as i64;
+
+    match unit {
+        "w" => Ok(Duration::weeks(time)),
+        "d" => Ok(Duration::days(time)),
+        "h" => Ok(Duration::hours(time)),
+        "m" => Ok(Duration::minutes(time)),
+        _ => Err("Unkown duration unit".to_owned())
+    }
 }
